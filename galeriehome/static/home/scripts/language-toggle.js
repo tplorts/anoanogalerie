d86575fa52e9c72bb7f4671c91f7ff160c634bcd
Ted Lorts <tplorts@gmail.com>
2014-01-14 19:55:21 +0900

    playing with fade in/out for lang switching

--- galeriehome/static/home/scripts/language-toggle.js
+++ galeriehome/static/home/scripts/language-toggle.js
@@ -1,9 +1,22 @@
-langtogg = $(".language-toggle");
-indicator = langtogg.find(".language-indicator");
-langtogg.find(".language-option").click( function() {
-    indicator.animate({
+langtogg = $(".ml-toggle");
+indicator = langtogg.find(".ml-indicator");
+langtogg.find(".ml-option").click( function() {
+    newProperties = {
         left: $(this).position().left + "px", 
         width: ($(this).width() + 7) + "px",
         height: ($(this).height() + 7) + "px"
-    });
+    };
+    indicator.animate(newProperties, 600);
+
+    lang = $(this).attr("lang");
+    mls = $(".ml");
+    former = mls.find(".ml-active-lang");
+    latter = mls.find("[lang='"+lang+"']");
+    if( former && latter ) {
+        former.fadeOut(400, function() {
+            former.removeClass("ml-active-lang");
+            latter.addClass("ml-active-lang");
+            latter.fadeIn(400);
+        });
+    }
 });
