acc829178dfdb22f8c9cfde14daa52d4b3ab1c55
Ted Lorts <tplorts@gmail.com>
2014-01-15 11:48:04 +0900

    added python index to pages

--- galeriehome/urls.py
+++ galeriehome/urls.py
@@ -4,6 +4,11 @@ from galeriehome import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^home/', views.index, name='home'),
+    url(r'^idea/', views.idea),
+    url(r'^schedule/', views.schedule),
+    url(r'^news/', views.news),
+    url(r'^guide/', views.guide),
+    url(r'^access/', views.access),
+
     url(r'^sandbox/', views.sandbox, name='sandbox'),
 )
--- galeriehome/views.py
+++ galeriehome/views.py
@@ -9,6 +9,23 @@ def index(request):
     return render(request, 'home/index.html', context)
 
 
+def idea(request):
+    return render(request, 'home/idea.html', {})
 
+def schedule(request):
+    return render(request, 'home/schedule.html', {})
+
+def news(request):
+    return render(request, 'home/news.html', {})
+
+def guide(request):
+    return render(request, 'home/guide.html', {})
+
+def access(request):
+    return render(request, 'home/access.html', {})
+
+
+
+# This sandbox is for experimenting with new creations
 def sandbox(request):
     return render(request, 'home/sandbox.html', {})
