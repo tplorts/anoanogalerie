f9d4900552b37913fc9918682d10e467db09f9eb
Ted Lorts <tplorts@gmail.com>
2014-02-20 17:20:46 +0900

    made script for compiling less and uploading static files to s3

---
+++ update-static-files.sh
@@ -0,0 +1,9 @@
+echo "~(^_^)~ Compiling LESS into CSS"
+cd galeriehome/static/home/styles
+lessc styles.less styles.css
+lessc multilingual.less multilingual.css
+lessc pages/access.less pages/access.css
+
+echo "~(^_^)~ Beginning upload of new static files"
+cd ../..
+aws s3 sync . s3://anoanogalerie.com/static    --acl public-read
