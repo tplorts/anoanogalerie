b57a41bfb6c4d611b7236cd9b88076e8e89dd5d8
Ted Lorts <tplorts@gmail.com>
2014-04-13 05:03:32 +0900

    url for blog redirects to the actual blog url now. not using the iframed template

--- galeriehome/urls.py
+++ galeriehome/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import RedirectView
 
 from galeriehome import views
+
+
+blog_redirect = RedirectView.as_view(url='http://anoanogalerie.blogspot.jp/')
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^concept/', views.concept, name='concept'),
     url(r'^schedule/', views.schedule, name='schedule'),
-    url(r'^news/', views.news, name='news'),
+    url(r'^news/', blog_redirect, name='news'),
     url(r'^guide/', views.guide, name='guide'),
     url(r'^access/', views.access, name='access'),
-
-    url(r'^sandbox/', views.sandbox, name='sandbox'),
 )
