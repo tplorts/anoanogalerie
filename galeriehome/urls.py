492fa4308e749b31e7858fdb747238dc0dff2f46
Ted Lorts <tplorts@gmail.com>
2014-05-31 15:04:23 -0400

    super simple link to PDF file of floor guide

--- galeriehome/urls.py
+++ galeriehome/urls.py
@@ -5,12 +5,15 @@ from galeriehome import views
 
 
 blog_redirect = RedirectView.as_view(url='http://anoanogalerie.blogspot.jp/')
+pdf_redirect = RedirectView.as_view(url='http://s3-ap-northeast-1.amazonaws.com/anoanogalerie-static/static/media/documents/ano-ano-gallerie-floor-guide.pdf')
+
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^concept/', views.concept, name='concept'),
     url(r'^schedule/', views.schedule, name='schedule'),
     url(r'^news/', blog_redirect, name='news'),
-    url(r'^guide/', views.guide, name='guide'),
+    url(r'^guide/$', views.guide, name='guide'),
+    url(r'^guide/pdf/$', pdf_redirect, name='Guide PDF'),
     url(r'^access/', views.access, name='access'),
 )
