190c8ce60a3214a19589f48db10f21a1b653002b
Ted Lorts <tplorts@gmail.com>
2014-06-06 17:27:26 -0400

    added a basic model for exhibitions and artists

--- galeriehome/models.py
+++ galeriehome/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Artist( models.Model ):
+    name = models.CharField( max_length=200 )
+    bio = models.TextField( blank=True )
+    website = models.URLField( blank=True )
+    email = models.EmailField( max_length=254, blank=True )
+
+    def __unicode__(self):
+        return self.name
+
+    
+class Exhibition( models.Model ):
+    title = models.CharField( max_length=200 )
+    brief = models.CharField( max_length=300, blank=True )
+    lengthy = models.TextField( blank=True )
+    start = models.DateField()
+    end = models.DateField()
+    hours = models.TextField()
+    artist = models.ForeignKey( Artist, null=True, blank=True )
+    picture = models.URLField( blank=True )
+
+    def __unicode__(self):
+        return self.title
