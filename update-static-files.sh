791739ea58870abef669fd3ee8868c52fb5e9af0
Ted Lorts <tplorts@gmail.com>
2014-10-14 13:20:04 -0400

    updated static routine to include everything in the proper way

--- update-static-files.sh
+++ update-static-files.sh
@@ -1,9 +1,12 @@
-echo "~(^_^)~ Compiling LESS into CSS"
+echo "~~ Compiling LESS into CSS"
 cd galeriehome/static/home/styles
 lessc styles.less styles.css
 lessc multilingual.less multilingual.css
 lessc pages/access.less pages/access.css
 
-echo "~(^_^)~ Beginning upload of new static files"
-cd ../..
-aws s3 sync . s3://anoanogalerie-static/static    --acl public-read
+echo "~~ collectstatic"
+cd ../../../../
+python manage.py collectstatic
+
+echo "~~ uploading new static files"
+aws s3 sync staticfiles/ s3://anoanogalerie-static/static    --acl public-read
